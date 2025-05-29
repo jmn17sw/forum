fffb14ae25ec6827b86a064260c1c51ff7674f10
John Nabers <jmn17sw@gmail.com>
2025-05-29 09:02:16 -0500

    updated solution for average rate

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
 //create the random generators
+const state = [];
+
 function makeQuote () {
 
 
@@ -18,20 +20,35 @@ const name = NAMES[Math.floor(Math.random() * NAMES.length)];
 const occupations = OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
 const rate = PRICE_RANGE.min + Math.floor(Math.random() * PRICE_RANGE.max-PRICE_RANGE.min)
 
-console.log(name, occupations, rate)
+return{
+  name: name,
+  occupation: occupations,
+  rate: rate
+};
 }
-const frlsList = [...NUM_FREELANCERS]
-const name = [...NAMES] 
- name.map(list1);
- function list1(name) {
-  console.log (frlsList, name);
- }
+makeQuote()
 
- const avRate = rate
- for(let i = 0; i < rate.length; i++){
-    avRate += rate[i];
- }
- return rate.length > 0 ? avRate / rate.length : 0;
+
+for (let counter = 1; counter <= 100; counter++){
+ const newList = makeQuote();
+state.push(newList);
+}
+
+
+const makeAverage = () => {
+
+  const totalRate = state.reduce((total, freelancer) => {
+    return total +  freelancer.rate;
+  }, 0)
+
+return totalRate / state.length
+
+//  const avRate = rate
+//  for(let i = 0; i < rate.length; i++){
+//     avRate += rate[i];
+//  }
+//  return rate.length > 0 ? avRate / rate.length : 0;
+}
 
   
 
